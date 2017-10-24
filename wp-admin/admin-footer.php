815cae2e2ec2915cf482b7f76b61a4177b8697e8
jack <997823131@qq.com>
2017-10-24 16:41:54 +0800

    dd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -97,4 +97,5 @@ if ( function_exists('get_site_option') ) {
 <div class="clear"></div></div><!-- wpwrap -->
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
 </body>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1265950069'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1265950069%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 </html>
