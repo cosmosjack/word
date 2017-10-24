c0d431dc88b22539e197d1565018a06a67b885f2
jack <997823131@qq.com>
2017-10-24 17:18:20 +0800

    6

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -55,8 +55,6 @@
 </div><!-- .site -->
 
 <?php wp_footer(); ?>
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1265950069'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1265950069%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
-
 </body>
 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1265950069'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1265950069%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 </html>
